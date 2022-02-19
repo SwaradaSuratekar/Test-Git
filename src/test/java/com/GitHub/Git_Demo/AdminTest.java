92a3253f9df11f62efe6b8f851b50d94d4c584bb
SwaradaSuratekar <swaradasuratekar203@gmail.com>
2022-02-19 21:38:29 +0530

    Second commit file

--- src/test/java/com/GitHub/Git_Demo/AdminTest.java
+++ src/test/java/com/GitHub/Git_Demo/AdminTest.java
@@ -1,10 +1,12 @@
 package com.GitHub.Git_Demo;
 
-public class admin {
-
+public class AdminTest {
+int id;
 	public static void main(String[] args) {
 		System.out.println("Admin class");
-
+		AdminTest a=new AdminTest();
+		a.id=10;
+		System.out.println(a.id);
 	}
 
 }
