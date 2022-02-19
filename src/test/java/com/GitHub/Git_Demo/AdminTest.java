8f9a0d707d639af9b14962206d0f60eaed17e441
SwaradaSuratekar <99752668+SwaradaSuratekar@users.noreply.github.com>
2022-02-19 21:47:01 +0530

    Updating code

--- src/test/java/com/GitHub/Git_Demo/AdminTest.java
+++ src/test/java/com/GitHub/Git_Demo/AdminTest.java
@@ -2,11 +2,13 @@ package com.GitHub.Git_Demo;
 
 public class AdminTest {
 int id;
+static String s= "Welcome";
 	public static void main(String[] args) {
 		System.out.println("Admin class");
 		AdminTest a=new AdminTest();
 		a.id=10;
 		System.out.println(a.id);
+		System.out.println(s);
 	}
 
 }
