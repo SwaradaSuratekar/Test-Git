c34dcc8ab714a41c0501cdb1e2795c5ca17a4367
SwaradaSuratekar <swaradasuratekar203@gmail.com>
2022-02-19 21:54:47 +0530

    login changes

--- src/test/java/com/GitHub/Git_Demo/login.java
+++ src/test/java/com/GitHub/Git_Demo/login.java
@@ -4,6 +4,10 @@ public class login {
 
 	public static void main(String[] args) {
 		System.out.println("Login class");
+		login l= new login();
+		l.m1();
 	}
-
+public void m1() {
+	System.out.println("Add username & password");
+}
 }
